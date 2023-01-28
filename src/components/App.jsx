c1a53e5a488c8d2fb090e35127d02ebdb106d161
Kyrylo Sulihan <paulhighrow@gmail.com>
2023-01-28 20:30:09 +0200

    Refactoring done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,63 +2,59 @@ import { SectionTitle } from './SectionTitle/SectionTitle';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Notification } from './Notification/Notification';
-import { Component } from 'react';
-
+import { useState } from 'react';
 
 const TITLES = {
   feedback: 'Please leave feedback',
   statistics: 'Statictics',
 };
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedbackHandler = evt => {
+    switch (evt.target.textContent) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
   };
 
-  feedbackHandler = evt => {
-    this.setState(prevState => {
-      return {
-        [evt.target.textContent]: prevState[evt.target.textContent] + 1,
-      };
-    });
-  };
+  const total = good + bad + neutral;
 
-  countTotalFeedback = state =>
-    Object.values(state).reduce((acc, value) => acc + value, 0);
+  const positiveFeedbackPercentage = ((good / total) * 100).toFixed(2);
 
-  countPositiveFeedbackPercentage = ({ good }) =>
-    ((good / this.countTotalFeedback(this.state)) * 100).toFixed(2);
-
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <>
-        <SectionTitle title={TITLES.feedback}>
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.feedbackHandler}
+  return (
+    <>
+      <SectionTitle title={TITLES.feedback}>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={feedbackHandler}
+        />
+      </SectionTitle>
+      <SectionTitle title={TITLES.statistics}>
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positiveFeedbackPercentage}
           />
-        </SectionTitle>
-        <SectionTitle title={TITLES.statistics}>
-          {this.countTotalFeedback(this.state) ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback(this.state)}
-              positivePercentage={this.countPositiveFeedbackPercentage(
-                this.state
-              )}
-            />
-          ) : (
-            <Notification message="There is no feedback yet!" />
-          )}
-        </SectionTitle>
-      </>
-    );
-  }
-}
-
+        ) : (
+          <Notification message="There is no feedback yet!" />
+        )}
+      </SectionTitle>
+    </>
+  );
+};
